b4c4476cac4677c120ddfef9b7f0b6bac5871b1f
Murat Sahin <yazilimogren1111@gmail.com>
2021-01-14 19:42:00 +0100

    second model

--- src/myapp/models.py
+++ src/myapp/models.py
@@ -3,8 +3,21 @@ from django.db import models
 # Create your models here.
 
 class Category(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, verbose_name="Category Name")
     
     def __str__(self):
         return self.name
+    class Meta :
+        verbose_name_plural = 'Categories'
+    
+class Quiz(models.Model):
+    title = models.CharField(max_length=100, verbose_name="Quiz Title")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
+    
+    class Meta :
+        verbose_name_plural = 'Quizzes'
     
