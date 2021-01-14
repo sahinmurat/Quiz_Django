a152939f6796329d9e4f8e2b63a6c3b0f9890c36
Murat Sahin <yazilimogren1111@gmail.com>
2021-01-14 20:39:10 +0100

    nested_admin used and models wrapped

--- src/myapp/admin.py
+++ src/myapp/admin.py
@@ -1,9 +1,25 @@
 from django.contrib import admin
+import nested_admin
 from .models import Category, Question,Quiz,Answer
+
+class AnswerInline(nested_admin.NestedTabularInline):
+    model = Answer
+    
+    
+class QuestinInline(nested_admin.NestedTabularInline):
+    model = Question
+    inlines = [AnswerInline]    
+
+class QuizAdmin(nested_admin.NestedModelAdmin):
+    model = Quiz
+    inlines = [QuestinInline]
+    
+
+
 
 admin.site.register(Category)
 admin.site.register(Question)
-admin.site.register(Quiz)
+admin.site.register(Quiz, QuizAdmin)
 admin.site.register(Answer)
 
 # Register your models here.
--- src/myprj/settings.py
+++ src/myprj/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'myapp',
-    'rest_framework'
+    'rest_framework',
+    'nested_admin'
 ]
 
 MIDDLEWARE = [
