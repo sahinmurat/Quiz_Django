28cf558acd155e4faebffb3114c1815af7d5db36
Murat Sahin <yazilimogren1111@gmail.com>
2021-01-16 18:42:06 +0100

    auth added

--- src/myapp/views.py
+++ src/myapp/views.py
@@ -3,16 +3,21 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Category, Question, Quiz
 from .serializers import CategorySerializer, CategoryDetailSerializer, QuestionSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
 # from .pagination import MyPagination
+
 
 
 class CategoryList(generics.ListAPIView):
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
+    permission_classes = [IsAuthenticated]
+    # authentication_classes = [TokenAuthentication, Sess]
 
 
 class CategoryDetail(generics.ListAPIView):
     serializer_class = CategoryDetailSerializer
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         queryset = Quiz.objects.all()
